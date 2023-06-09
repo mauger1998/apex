1407896f128be04d28959232dae013f09b91828b
mauger1998 <110930037+mauger1998@users.noreply.github.com>
2023-06-09 09:55:54 +0100

    Push newest

--- index.js
+++ index.js
@@ -13,3 +13,23 @@ document.addEventListener("click", (e) => {
         dropdown.classList.remove("active")
     })
   })
+
+
+// CMS
+
+
+
+// Create the URL for your endpoint and add the query
+let URL = `https://fn92pamt.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20'galleryImages'%5D%7B%0A%20%20name%2C%0A%20%20%22imageUrl%22%3A%20img.asset-%3Eurl%0A%7D%0A`;
+
+fetch(URL)
+        .then((res) => res.json())
+        .then(({ result }) => {
+          if (result.length > 0) {
+            const galleryImages = document.querySelectorAll(".gallery-grid img")
+            result.forEach((result, index) => {
+              galleryImages[index].src = result.imageUrl
+            });
+          }
+        })
+        .catch((err) => console.error(err));
